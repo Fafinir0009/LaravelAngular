50ed29f45e795d186a76c2e29018125261852da0
Fafinir0009 <davijoaquim0009@gmail.com>
2026-05-27 12:27:03 -0300

    criação do aluno e prof function

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -19,3 +19,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('teste', function() {});
+
+Route::get('teste', function(){
+    return "Hello World";
+});
+
+Route::prefix('aluno')->group (function(){
+    Route::get('listar', (['App\Http\Controllers\AlunoController', 'listar']));
+    Route::get('adicionar', (['App\Http\Controllers\AlunoController', 'listar']));
+    Route::get('remove', (['App\Http\Controllers\AlunoController', 'listar']));
+    Route::get('criar', (['App\Http\Controllers\AlunoController', 'listar']));
+
+});
+
+Route::prefix('professor')->group (function(){
+     Route::get('listar', (['App\Http\Controllers\ProfessorController', 'listar']));
+    Route::get('listar', (['App\Http\Controllers\ProfessorController', 'listar']));
+    Route::get('adicionar', (['App\Http\Controllers\ProfessorController', 'listar']));
+    Route::get('remove', (['App\Http\Controllers\ProfessorController', 'listar']));
+    Route::get('criar', (['App\Http\Controllers\ProfessorController', 'listar']));
+
+});
